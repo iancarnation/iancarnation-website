e47e8ee6aefbb974d2555c887ac799171b50ebde
Ian Rich <iancarnation@gmail.com>
2018-09-05 00:10:18 -0700

    added resume pdf to about

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import React from "react";
+import resume from "../files/Ian_Rich-Resume.pdf"
 
 export default ({data}) => (
     <div>
@@ -9,7 +10,14 @@ export default ({data}) => (
             <em>Intrepid Technician  .  Insatiable Learner  .  Intriguing Artist  .  Infinite Smiles-Provider</em>
         </p>
         Hello, I'm an educator and technical artist excited to support your creative and computational needs!
-  </div>
+        {/*<embed src={resume} type="application/pdf" width="100%" height="900px" />*/}
+        <p>
+            <br></br>
+            <a href={resume}>View Resume</a>
+        </p>
+    </div>
+    
+
 );
 
 export const query = graphql`
